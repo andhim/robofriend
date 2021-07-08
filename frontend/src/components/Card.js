84c0e627bf4d20aa715b734dcd2f0e4234e2af51
Jaeyoung Park <andhim@Jaeyoungs-MacBook-Pro.local>
2021-07-08 10:25:54 -0600

    to my macbook

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (    
+        <div>
+            <img src='' alt='img'/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>haha@gmail.come</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
